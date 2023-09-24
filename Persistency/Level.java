4b5f21b1fd21de6e5a8e537d806ed807c1c555f2
salvadalvi <salvadalvi@ecs.vuw.ac.nz>
2023-09-24 21:23:38 +1300

    integrated domain class with Persistency

--- Persistency/Level.java
+++ Persistency/Level.java
@@ -1,29 +1,29 @@
 package Persistency;
 import java.util.ArrayList;
-
+import Domain.*;
 class Level{
 
     private ArrayList<Tile> tiles;
-    private ArrayList<Item> items;
-    private ArrayList<Entity> Entites;
+    private ArrayList<Entity> entites;
+    private Player player;
 
     public Level(){
         tiles = new ArrayList<>();
-        Items = new ArrayList<>();
-        Entites = new ArrayList<>();
+        entites = new ArrayList<>();
+        player = new Player(new Coord(0,0));
     }
 
-    public ArrayList<Tile> getTiles(){
-        return list;
-    }
-
-    public ArrayList<Item> getItems(){
-        return item;
+    public ArrayList<Tile> getItems(){
+        return  tiles;
     }
 
     public ArrayList<Entity> getEntites(){
         return entites;
     }
 
+    public Player getPlayer(){
+
+        return player;
+    }
 
 }
--- Persistency/Persistency.java
+++ Persistency/Persistency.java
@@ -2,13 +2,14 @@
 package Persistency;
 
 
-class Persistency{
+public class Persistency{
 
     public static Level loadLevel(String path){
 
         return new Level();
     }
 
+    
 
 }
 
