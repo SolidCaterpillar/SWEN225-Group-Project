c7befc1b2b663398380e60f71f07457533da4081
salvadalvi <salvadalvi@ecs.vuw.ac.nz>
2023-09-27 00:52:44 +1300

    Implemented template for a saving levels into jsons

--- Persistency/Test.java
+++ Persistency/Test.java
@@ -3,6 +3,9 @@ package Persistency;
 public class Test {
     public static void main(String[] args){
         Level l1 = Persistency.loadLevel1();
-        System.out.println(l1.board());
+
+       Persistency.saveLevel("newFile.json",l1);
+
+
     }
 }
