2235356566ffaf9fd0e22c9cc26e05b7c7dfa1a1
Takoda Teal <tealtako@ecs.vuw.ac.nz>
2023-09-28 13:08:13 +1300

    v1.1: Added test checking for domain, testWallCollisions, testWrongPosition, testInvalidInput, testValidLevels, testInvalidLevels

--- Fuzz/Fuzz_Testing.java
+++ Fuzz/Fuzz_Testing.java
@@ -1,7 +1,12 @@
 package test.nz.ac.wgtn.swen225.lc.fuzz;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.Test;
 
 public class Fuzz_Testing {
 	// NEED TO ENSURE THIS WAY OF TESTING ADHERES TO BRIEF
@@ -34,6 +39,10 @@ public class Fuzz_Testing {
 		// Tests scenarios generic to both levels
 		if (level == 1 || level == 2) {
 			testWallCollisions();
+			testWrongPosition();
+			testInvalidInput();
+			testValidLevels();
+			testInvalidLevels();
 
 		}
 
@@ -72,8 +81,14 @@ public class Fuzz_Testing {
 
 	// A lot of redundancy here if everything is done this way improve later
 	// (list of actions to loop through?)
+	// Can do similar for all various tile interactions
+	
+	
+	@Test
 	public void testWallCollisions() {
 		collisionBoard();
+		
+		//FORCE RANDOM INPUT ONCE WORKING
 
 		// Test collisions from all directions: up, down, left, right
 
@@ -94,6 +109,85 @@ public class Fuzz_Testing {
 		assertTrue(rightCollision); // Expecting a collision with the wall
 
 	}
+	
+	
+	
+	//DOMAIN TESTS
+	
+	
+	
+	@Test
+	public void testWrongPosition() {
+	    collisionBoard();
+	    
+	    // Test a bunch of invalid coordinates
+	    // This is accessed through app, domain is responsible for coord
+
+	    // Invalid coordinates that are outside the board boundaries
+	    Coord invalidCoord1 = new Coord(-1, -1);
+	    Coord invalidCoord2 = new Coord(4, 4);
+	    Coord invalidCoord3 = new Coord(0, -1);
+	    Coord invalidCoord4 = new Coord(-1, 0);
+
+	    // Test if these coordinates are out of bounds - DONE IN DOMAIN
+	    boolean isInvalid1 = checkInbound(invalidCoord1.x(), invalidCoord1.y());
+	    boolean isInvalid2 = checkInbound(invalidCoord2.x(), invalidCoord2.y());
+	    boolean isInvalid3 = checkInbound(invalidCoord3.x(), invalidCoord3.y());
+	    boolean isInvalid4 = checkInbound(invalidCoord4.x(), invalidCoord4.y());
+
+	    // Assert that all of these coordinates are invalid
+	    assertFalse(isInvalid1);
+	    assertFalse(isInvalid2);
+	    assertFalse(isInvalid3);
+	    assertFalse(isInvalid4);
+	}
+
+	
+	@Test
+	public void testInvalidInput() {
+	    collisionBoard();
+
+	    // Create an array of invalid input keys
+	    char[] invalidKeys = {'x', 'z', '1', '!', '@'};
+
+	    // Iterate through each invalid key and test it
+	    for (char invalidKey : invalidKeys) {
+	        try {
+	            move(invalidKey); // Attempt to move with invalid input - DONE IN DOMAIN
+	            fail("Expected IllegalArgumentException for invalid input: " + invalidKey);
+	        } catch (IllegalArgumentException e) {
+	            // This is expected, as an exception should be thrown for invalid input
+	            // No need to add any specific assertions here
+	        }
+	    }
+	}
+	
+	
+
+
+	    @Test
+	    public void testValidLevels() {
+	        // Test valid levels (0, 1, 2)
+	        for (int level = 0; level <= 2; level++) {
+	            assertDoesNotThrow(() -> new Board(level, new Tile[26][26]));
+	        }
+	    }
+
+	    
+	    @Test
+	    public void testInvalidLevels() {
+	        // Test invalid levels (< 0 and > 2)
+	        assertThrows(IllegalArgumentException.class, () -> new Board(-1, new Tile[26][26]));
+	        assertThrows(IllegalArgumentException.class, () -> new Board(3, new Tile[26][26]));
+	    }
+	
+	
+	    public void enemyNullPosCheck() {
+	    	
+	    	
+	    }
+	    
+	    
 
 	public static void main(String[] args) {
 		Fuzz_Testing fuzzTester = new Fuzz_Testing();
