7f494436ca641536f59d3480d29576968bd6a866
Ricky Fong <fongrick@perretts-corner.ecs.vuw.ac.nz>
2023-10-05 14:01:05 +1300

    update the recorder

--- nz/ac/wgtn/swen225/lc/recorder/Recorder.java
+++ nz/ac/wgtn/swen225/lc/recorder/Recorder.java
@@ -1,4 +1,7 @@
 package nz.ac.wgtn.swen225.lc.recorder;
+import nz.ac.wgtn.swen225.lc.domain.Entity.Enemy;
+import nz.ac.wgtn.swen225.lc.domain.Entity.Player;
+import nz.ac.wgtn.swen225.lc.domain.Tile.Tile;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.io.FileWriter;
@@ -8,70 +11,39 @@ import java.nio.file.Paths;
 import java.util.*;
 
 import nz.ac.wgtn.swen225.lc.app.*;
+
+/**
+ * @author Ricky Fong 300500545
+ *
+ */
 public class Recorder {
-    private static final String GAME_STATE_FILE = "game_state.json";
-    List<GameState> recorder; // collecting the game state
 
-    private static class GameState{
-        int timer; int currentLevel; String chipsText;
+    private JSONArray record;
 
-        public GameState(int timer, int currentLevel, String chipsText){
-            this.timer = timer;
-            this.currentLevel = currentLevel;
-            this.chipsText = chipsText;
-        }
+    public Recorder() { this.record = new JSONArray(); }
+
+    /**
+     * record the game everytime the gui redraw
+     * @param currentLevel record the current level of the game
+     * @param timer record the timer of the ongoing game
+     */
+    //public void setRecord( int currentLevel, int timer, Tile[][] maze){
+    public void setRecord(int currentLevel, int timer) {
+        JSONObject jsonGameState = new JSONObject();
+        jsonGameState.put("currentLevel", currentLevel);
+        jsonGameState.put("timer", timer);
+        //jsonGameState.put("maze", maze); // use maze's toString to store in json but in 2d
+        record.put(jsonGameState);
     }
 
-    public Recorder() {
-        this.recorder = new ArrayList<>();
-    }
-
-    // testing saving a file from the App
-    public void saveGameStateFile(int timer, int currentLevel, String chipsText) {
-        GameState gameState = new GameState( timer,  currentLevel,  chipsText);
-        recorder.add(gameState);
-        JSONArray jsonGameHistory = new JSONArray();
-        for(GameState state : recorder) {
-            JSONObject jsonGameState = new JSONObject();
-            jsonGameState.put("timer", state.timer);
-            jsonGameState.put("currentLevel", state.currentLevel);
-            jsonGameState.put("chipsText", state.chipsText);
-            jsonGameHistory.put(jsonGameState);
-        }
-
-        try (FileWriter fileWriter = new FileWriter(GAME_STATE_FILE)) {
-            fileWriter.write(jsonGameHistory.toString(4)); // Indent with 4 spaces for readability
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // loading a json file
-    public static void loadGameStateFile() {
-        try {
-            String gameStateJson = new String(Files.readAllBytes(Paths.get(GAME_STATE_FILE)));
-            JSONArray recordGame = new JSONArray(gameStateJson);
-
-            for (int i = 0; i < recordGame.length(); i++) {
-                JSONObject gameState = (JSONObject) recordGame.get(i);
-                int timer = gameState.getInt("timer");
-                int currentLevel = gameState.getInt("currentLevel");
-                String chipsText = gameState.getString("chipsText");
-
-                //printing the statement to test it out
-                System.out.println("Timer : " +timer);
-                System.out.println("currentLevel : " +currentLevel);
-                System.out.println("chipsText : " +chipsText);
-                System.out.println();
-
-                // Sleep for 2 seconds (2000 milliseconds)
-                Thread.sleep(1000);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+    /**
+     * Save the record game when player finish or end (quit) the game
+     * @param fileName record the current level of the game
+     */
+    public void saveAsFile(String fileName) throws IOException {
+        FileWriter fileWriter = new FileWriter(fileName);
+        fileWriter.write(record.toString());
+        fileWriter.close();
+        System.out.println("Game state saved successfully to " + fileName);
     }
 }
