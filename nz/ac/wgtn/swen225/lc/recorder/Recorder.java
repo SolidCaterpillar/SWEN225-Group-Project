8ea908ffea43157857d06fcafaedf66eb470cc5a
Ricky Fong <fongrick@barretts.ecs.vuw.ac.nz>
2023-10-05 21:13:00 +1300

    Updated the setRecord method to record a 2d maze

--- nz/ac/wgtn/swen225/lc/recorder/Recorder.java
+++ nz/ac/wgtn/swen225/lc/recorder/Recorder.java
@@ -26,14 +26,30 @@ public class Recorder {
      * record the game everytime the gui redraw
      * @param currentLevel record the current level of the game
      * @param timer record the timer of the ongoing game
+     * @param maze record the 2d maze of the ongoing game
      */
-    //public void setRecord( int currentLevel, int timer, Tile[][] maze){
-    public void setRecord(int currentLevel, int timer) {
+    public void setRecord( int currentLevel, int timer, Tile[][] maze){
         JSONObject jsonGameState = new JSONObject();
         jsonGameState.put("currentLevel", currentLevel);
         jsonGameState.put("timer", timer);
-        //jsonGameState.put("maze", maze); // use maze's toString to store in json but in 2d
-        record.put(jsonGameState);
+
+        JSONArray mazeArray = new JSONArray(); // store maze in 2d array
+
+        for (Tile[] row : maze) {
+            JSONArray rowArray = new JSONArray();
+            for (Tile tile : row) {
+                JSONObject tileObject = new JSONObject();
+                //tileObject.put(tile); // need to put a value or toString value from domain
+               //tileObject.put(tile);
+                rowArray.put(tileObject);
+            }
+            mazeArray.put(rowArray);
+        }
+
+        // Add the 2d maze array to the JSON object
+        jsonGameState.put("Maze", mazeArray);
+
+        record.put(jsonGameState); // add all the JSON object in the JSON array
     }
 
     /**
@@ -42,7 +58,7 @@ public class Recorder {
      */
     public void saveAsFile(String fileName) throws IOException {
         FileWriter fileWriter = new FileWriter(fileName);
-        fileWriter.write(record.toString());
+        fileWriter.write(record.toString(4));
         fileWriter.close();
         System.out.println("Game state saved successfully to " + fileName);
     }
