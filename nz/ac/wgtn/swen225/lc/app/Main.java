20d7c7808c41597b605d4e0f8c71e68411b708e6
Emmanuel De Vera <deveremma@chariot.ecs.vuw.ac.nz>
2023-10-10 15:09:01 +1300

    Tidied up App Module Code with appropriate Javadoc comments

--- nz/ac/wgtn/swen225/lc/app/Main.java
+++ nz/ac/wgtn/swen225/lc/app/Main.java
@@ -1,33 +1,33 @@
 package nz.ac.wgtn.swen225.lc.app;
 
-
+/*
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import nz.ac.wgtn.swen225.lc.domain.*;
 import nz.ac.wgtn.swen225.lc.domain.Tile.*;
 import nz.ac.wgtn.swen225.lc.domain.Entity.*;
 import nz.ac.wgtn.swen225.lc.persistency.*;
 import nz.ac.wgtn.swen225.lc.renderer.*;
-//import nz.ac.wgtn.swen225.lc.recorder.*;
+import nz.ac.wgtn.swen225.lc.recorder.*;
+*/
 
-
+/**
+ * This is the Main class that runs the game.
+ * This is named as per the specifications
+ */
 public class Main {
-    private GUI gui;
-
+    /**
+     * Creating a Main class will immediately create a GUI object
+     */
     public Main() {
-        gui = new GUI();
-
+        GUI gui = new GUI();
     }
-
+    /**
+     * In the main method, making a Main object will make the GUI object
+     */
     public static void main(String[] args) {
         Main game = new Main();
-
         //game.playGame(); // Start the game loop
     }
 }
-
-
-
-
